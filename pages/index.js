18a9ad924e931e432112b5cb17212c8a63d69ce9
Varun Shihara <varunshihara@gmail.com>
2020-07-22 16:21:14 +0530

    removed ip

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+// git remote add origin https://github.com/varunshihara/hackerpost-next.git
 // git push -u origin master
 import Head from "next/head";
 
@@ -13,34 +14,8 @@ export default function Home() {
       <main>
         <h1 className="title textCenter">HACKER POST</h1>
         <p className="textCenter">Working on something cool...coming soon.</p>
-        <p className="textCenter">YOUR IP : </p>
+        {/* <p className="textCenter">YOUR IP : </p> */}
       </main>
-
-      <style jsx>{`
-        .container {
-          // min-height: 100vh;
-          // padding: 0 0.5rem;
-          // display: flex;
-          // flex-direction: column;
-          // justify-content: center;
-          // align-items: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        // html,
-        // body {
-        //   padding: 0;
-        //   margin: 0;
-        //   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-        //     Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-        //     sans-serif;
-        // }
-
-        // * {
-        //   box-sizing: border-box;
-        // }
-      `}</style>
     </div>
   );
 }
